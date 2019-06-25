6ea439df61ea2c226a676341b6b49d67d4d9f11c
Henry Darnell <hjdarnel@users.noreply.github.com>
2019-06-25 16:20:21 -0500

    Write and remove temp files

--- text.js
+++ text.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const text2png = require('text2png');
 const gm = require('gm').subClass({ imageMagick: true });;
 
@@ -20,26 +21,28 @@ const createImage = (text = 'yeet', res) => {
             return res.end("File not found.");
         }
 
-        gm('./assets/lines.png').resize(value.width).write('./assets/resized_lines.png', (err) => {
+        gm('./assets/lines.png').resize(value.width).write('/tmp/resized_lines.png', (err) => {
             if (err) {
                 res.writeHead(404);
                 return res.end("File not found.");
             }
 
-            gm(textBuffer).append('./assets/resized_lines.png').toBuffer(function (err, val) {
+            gm(textBuffer).append('/tmp/resized_lines.png').toBuffer(function (err, val) {
                 if (err || !val) {
                     res.writeHead(404);
                     return res.end("File not found.");
                 }
 
-                gm(val).command('convert').in('-virtual-pixel',  'none', '-distort', 'Arc', arcDegrees).write('./assets/out.png', (err) => {
+                gm(val).command('convert').in('-virtual-pixel',  'none', '-distort', 'Arc', arcDegrees).write('/tmp/out.png', (err) => {
                     if (err) {
                         res.writeHead(404);
                         return res.end("File not found.");
                     }
 
-                    gm('./assets/out.png').rotate('none', rotationDegrees).toBuffer((err, value) => {
+                    gm('/tmp/out.png').rotate('none', rotationDegrees).toBuffer((err, value) => {
                         if (!err) console.log('Written composite image.');
+                        fs.unlink('/tmp/out.png', () => {});
+                        fs.unlink('/tmp/resized_lines.png', () => {});
                         res.setHeader('Content-Type', 'image/png');
                         res.end(value);
                     });
